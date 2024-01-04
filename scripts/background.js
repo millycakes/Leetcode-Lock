dc7112f848c5ce4d27b03c206561cbd605a1dc19
Emily Wang <egw7501@nyu.edu>
2024-01-04 08:47:58 +0800

    added redirecting on access to blacklisted sites

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,20 @@
 chrome.storage.local.set({
-    blacklistWebsites: ["youtube.com"]
+    blacklistWebsites: ["youtube.com"],
+    lastSolved: -1
 })
+
+chrome.tabs.onUpdated.addListener((tab) => {
+    chrome.storage.local.get(["lastSolved", "blacklistWebsites"], function (item) {
+        console.log(tab.url);
+        if (tab.url) {
+            for (let i = 0; i<item["blacklistWebsites"].length; i++) {
+                if (tab.url.includes(item["blacklistWebsites"][i])) {
+                    if (item["lastSolved"]==-1 || new Date().getDate()!=item["lastSolved"].getDate()) {
+                        chrome.tabs.update(tab.id, {url: 'http://leetcode.com'});
+                    }
+                }
+            }
+        }
+    })
+  });
+
