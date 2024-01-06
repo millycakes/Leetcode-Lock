0da1ed0e66745f55e7ef3681865760437f62e988
Emily Wang <egw7501@nyu.edu>
2024-01-06 10:05:41 +0800

    redirect to random leetcode problem based on tags

--- scripts/background.js
+++ scripts/background.js
@@ -1,20 +1,58 @@
 chrome.storage.local.set({
-    blacklistWebsites: ["youtube.com"],
-    lastSolved: -1
+    blacklistWebsites: ["instagram.com"],
+    lastSolved: -1,
+    tags:['string', 'hash-table']
 })
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    chrome.storage.local.get(["lastSolved", "blacklistWebsites"], function (item) {
+    chrome.storage.local.get(["lastSolved", "blacklistWebsites", "tags"], function (item) {
         console.log(tab.url);
         if (tab.url) {
+            const matchid = tab.id;
             for (let i = 0; i<item["blacklistWebsites"].length; i++) {
                 if (tab.url.includes(item["blacklistWebsites"][i])) {
                     if (item["lastSolved"]==-1 || new Date().getDate()!=item["lastSolved"].getDate()) {
-                        chrome.tabs.update(tab.id, {url: 'http://leetcode.com'});
+                        let url = 'http://leetcode.com/problemset/?page=1&status=NOT_STARTED';
+                        if (item["tags"].length>0) {
+                            url+="&topicSlugs=";
+                            for (let i = 0; i<item["tags"].length; i++) {
+                                if (i==item["tags"].length-1) {
+                                    url+=item["tags"][i];
+                                }
+                                else {
+                                    url+=item["tags"][i]+'%2C';
+                                }
+                            }
+                        }
+                        chrome.tabs.update(tabId, { url: url }, function() {
+                            let messageSent = false;
+                        
+                            function checkTabStatus(tabId) {
+                                if (messageSent) return;
+                        
+                                chrome.tabs.get(tabId, function(tab) {
+                                    if (tab.status === "complete" && !messageSent) {
+                                        chrome.tabs.sendMessage(tabId, { action: "selectQ" });
+                                        messageSent = true; 
+                                    } else {
+                                        setTimeout(() => checkTabStatus(tabId), 100); 
+                                    }
+                                });
+                            }
+                        
+                            checkTabStatus(tabId);
+                        });
+                        
                     }
                 }
             }
         }
     })
   });
+
+  chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.redirect) {
+        chrome.tabs.update(sender.tab.id, { url: request.redirect });
+    }
+});
 
---
+++ scripts/contentScript.js
@@ -0,0 +1,20 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "selectQ") {
+        console.log("Message received", request);
+
+        function checkAndExecute() {
+            const rowgroups = document.querySelectorAll('[role="rowgroup"]');
+            if (rowgroups.length > 1) {
+                clearInterval(checkInterval);
+                const rowgroup = rowgroups[1];
+                const random = Math.floor(Math.random() * rowgroup.childNodes.length);
+                const questionLink = rowgroup.childNodes[random].childNodes[1].firstChild.firstChild.firstChild.firstChild.firstChild.href;
+                chrome.runtime.sendMessage({ redirect: questionLink });
+            }
+        }
+
+        if (typeof checkInterval === 'undefined') {
+            var checkInterval = setInterval(checkAndExecute, 1000);
+        }
+    }
+});
