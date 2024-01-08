6e735f3f9f099eead3d32f1b3fd7418f6bbf0b8a
Emily Wang <egw7501@nyu.edu>
2024-01-08 21:47:56 +0800

    stopped redirecting after solving leetcode

--- scripts/background.js
+++ scripts/background.js
@@ -6,12 +6,40 @@ chrome.storage.local.set({
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     chrome.storage.local.get(["lastSolved", "blacklistWebsites", "tags"], function (item) {
-        console.log(tab.url);
         if (tab.url) {
-            const matchid = tab.id;
+            if (new RegExp("https://leetcode.com/problems/[^/]+/submissions/").test(tab.url)) {
+                const problem = tab.url.split("/")[4];
+                const request = `https://leetcode.com/api/submissions/${problem}/`;
+                fetch(request)
+                    .then(response => response.json())
+                    .then(data => {
+                        for (let i = 1; i < data.submissions_dump.length; i++) {
+                            if (data.submissions_dump[i].status_display === "Accepted") {
+                                console.log("question already solved");
+                                return;
+                            }
+                        }
+
+                        const id = data.submissions_dump[0].id;
+                        const checkUrl = `https://leetcode.com/submissions/detail/${id}/check/`;
+                        const interval = setInterval(() => {
+                            fetch(checkUrl)
+                                .then(response => response.json())
+                                .then(data => {
+                                    if (data.state === "SUCCESS" && data.status_msg === "Accepted") {
+                                        chrome.storage.local.set({lastSolved: new Date().toISOString()});
+                                        clearInterval(interval);
+                                    }
+                                })
+                                .catch(error => console.error('Error:', error));
+                        }, 1000);
+                    })
+                    .catch(error => console.log('Error:', error));
+            }
+
             for (let i = 0; i<item["blacklistWebsites"].length; i++) {
                 if (tab.url.includes(item["blacklistWebsites"][i])) {
-                    if (item["lastSolved"]==-1 || new Date().getDate()!=item["lastSolved"].getDate()) {
+                    if (item["lastSolved"]==-1 || new Date().getDate()!=new Date(item["lastSolved"]).getDate()) {
                         let url = 'http://leetcode.com/problemset/?page=1&status=NOT_STARTED';
                         if (item["tags"].length>0) {
                             url+="&topicSlugs=";
@@ -39,7 +67,6 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                                     }
                                 });
                             }
-                        
                             checkTabStatus(tabId);
                         });
                         
